f187c3d317ee9528cb251414f25c0a5ea2e96027
Saul Isai Alvarenga Bonilla <pul98alvarenga@gmail.com>
2018-11-06 02:06:49 -0600

    segunda version

---
+++ enviar.php
@@ -0,0 +1,20 @@
+<?php   
+
+$nombre + $_POST['name'];
+$correo + $_POST['email'];
+$mensaje + $_POST['message'];
+
+// Datos para el correo
+
+$destinatario = "pul98alvarenga@gmail.com";
+$asunto = "Contacto desde la Web - CAPCYSA de RL"
+
+$carta = "Servicio de Consultas de CAPCYSA \n";
+$carta .= "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "mensaje: $mensaje]";
+
+//enviando mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:message-send.html');
+?>
